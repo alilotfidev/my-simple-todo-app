5c2c869ddfb64693b511d9a4c07150c583c78388
alilotfi.dev <lotfi26.ali@gmail.com>
2020-07-12 13:29:36 +0430

    its working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Todos from './Todos';
+import AddForm from './AddForm';
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy milk!' },
+      { id: 2, content: 'apply for frankfort! ;)' },
+    ]
+  }
+  deleteTodo = (id) => {
+    const newTodos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: newTodos
+    });
+  }
+  addTodo = (todo) => {
+    todo.id = this.state.todos[this.state.todos.length -1].id + 1;
+    const newTodos = [...this.state.todos , todo];
+    this.setState({
+      todos: newTodos
+    });
+  }
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddForm addTodo={this.addTodo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
